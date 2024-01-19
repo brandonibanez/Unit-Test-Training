ec929ff2e059bfb1e72194bd5d672e7c3e6c2e4d
brandonibanez <ibanezbrandonn@gmail.com>
2024-01-19 18:47:25 +0800

    Spring Boot REST APIs Unit Testing

--- 4.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
+++ 4.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
@@ -136,9 +136,9 @@ public class StudentAndGradeServiceTest {
         assertNotNull(gradebookCollegeStudentTest.getStudentGrades().getScienceGradeResults());
         assertNotNull(gradebookCollegeStudentTest.getStudentGrades().getHistoryGradeResults());
 
-        assertEquals("Eric", gradebookCollegeStudentTest.getFirstname());
-        assertEquals("Roby", gradebookCollegeStudentTest.getLastname());
-        assertEquals("eric.roby@luv2code_school.com", gradebookCollegeStudentTest.getEmailAddress());
+        assertEquals("Rabby", gradebookCollegeStudentTest.getFirstname());
+        assertEquals("Dabby", gradebookCollegeStudentTest.getLastname());
+        assertEquals("test@test.com", gradebookCollegeStudentTest.getEmailAddress());
 
     }
 
